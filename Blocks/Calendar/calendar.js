19cfad6464a2cece6f01d473d7a8155a9fc9884c
Yurchak Veronika <yurchakveronika1997@gmail.com>
2020-09-16 23:51:33 +0300

    Added calendar

---
+++ Blocks/Calendar/calendar.js
@@ -0,0 +1,13 @@
+$( function() {
+    $( ".datepicker" ).datepicker();
+} );
+jQuery(function($){
+    $.datepicker.regional['ru'] = {
+        monthNames: ['Яварь', 'Февраль', 'Март', 'Апрель',
+            'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь',
+            'Октябрь', 'Ноябрь', 'Декабрь'],
+        dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+        firstDay: 1,
+    };
+    $.datepicker.setDefaults($.datepicker.regional['ru']);
+});
