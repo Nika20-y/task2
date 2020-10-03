c57b68bbe96509e8fed63a3efd1a66b794590e3d
Yurchak Veronika <yurchakveronika1997@gmail.com>
2020-10-03 23:36:22 +0300

    Added checkbox list

---
+++ Blocks/checkboxList/checkboxList.js
@@ -0,0 +1,13 @@
+let checkboxList = document.getElementsByClassName("checkboxList__wrapper")[0];
+checkboxList.addEventListener("click", function() {
+    this.classList.toggle("active");
+    let checkbox = this.nextElementSibling;
+    let icon = document.getElementsByClassName("material-icons")[0];
+    if (checkbox.style.display === "block") {
+        checkbox.style.display = "none";
+        icon.style.transform ="none";
+    } else {
+        checkbox.style.display = "block";
+        icon.style.transform ="rotate(180deg)";
+    }
+});
