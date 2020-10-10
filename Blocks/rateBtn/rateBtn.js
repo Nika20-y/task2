7974f84a1c52aa552b9ffa3cae95c3c581e01e1b
Yurchak Veronika <yurchakveronika1997@gmail.com>
2020-10-10 14:30:51 +0300

    Made rate button

---
+++ Blocks/rateBtn/rateBtn.js
@@ -0,0 +1,19 @@
+let rateBtn = document.getElementsByClassName('rateBtn__icon');
+for(let i=0; i<rateBtn.length; i++) {
+        rateBtn[i].onclick = function () {
+            let star = document.getElementsByClassName('material-icons')[i].innerHTML;
+            if(star ==='star_border'){
+                star = 'star';
+                for (let a=0; a<=i; a++){
+                    document.getElementsByClassName('material-icons')[a].innerHTML=star;
+                }
+            }
+            else{
+                star='star_border'
+                for (let a=i+1; a<rateBtn.length; a++){
+                    document.getElementsByClassName('material-icons')[a].innerHTML=star;
+                }
+            }
+        }
+
+}
