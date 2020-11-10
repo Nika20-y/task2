e72574cd9eb4b8272d97ae56f3549fe78d80121a
Yurchak Veronika <yurchakveronika1997@gmail.com>
2020-11-10 20:49:25 +0300

    Correct mistakes in dropdown's js code

--- src/Blocks/Dropdown/dropdown.js
+++ src/Blocks/Dropdown/dropdown.js
@@ -1,71 +1,72 @@
+var acc = document.getElementsByClassName("dropdown");
+let allGuests= [];
 
-    var acc = document.getElementsByClassName("dropdown");
-    let allGuests= [];
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    })
+}
+document.addEventListener('DOMContentLoaded', function(){
+    setTitle();
+}, false);
 
-    for (let i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function () {
-            this.classList.toggle("active");
-            var panel = this.nextElementSibling;
-            if (panel.style.display === "block") {
-                panel.style.display = "none";
-            } else {
-                panel.style.display = "block";
-            }
-        })
+let quantity = []
+let minus = []
+let plus = []
+let number = document.getElementsByClassName('dropdown__list')
+let counter= [];
+
+for (let i = 0; i < acc.length; i++){
+    allGuests[i]=0;
+    let counter1 = []
+    let a = 0;
+    while (a < 3){
+        let number = document.getElementsByClassName('dropdown__list')
+        counter1.push(number[i].getElementsByClassName("dropdown__visitors")[a].value)
+        allGuests[i]=allGuests[i]+Number(counter1[a]);
+        a++;
     }
-    document.addEventListener('DOMContentLoaded', function(){
-        setTitle();
-    }, false);
+    counter.push([counter1]);
+}
 
-    let quantity = []
-    let minus = []
-    let plus = []
 
-    for (let i = 0; i < acc.length; i++){
-        allGuests[i]=0;
-        let counter = []
-        let a = 0;
-        while (a < 3){
-            let number = document.getElementsByClassName('dropdown__list')
-            counter.push(number[i].getElementsByClassName("dropdown__visitors")[a].value)
-            allGuests[i]=allGuests[i]+Number(counter[a]);
-            a++;
+for (let i = 0; i < acc.length; i++) {
+    quantity = number[i].getElementsByClassName("dropdown__visitors");
+    minus = number[i].getElementsByClassName("dropdown__minus");
+    plus = number[i].getElementsByClassName("dropdown__plus");
+    for (let x = 0; x < 3; x++) {
+        counter[i][x]= quantity[x].value;
+        plus[x].onclick = function () {
+            if (counter[i][x] < 100) {
+                counter[i][x]++;
+                number[i].getElementsByClassName("dropdown__visitors")[x].value = counter[i][x];
+                allGuests[i] = allGuests[i]+1;}
+            setTitle();
+
+        };
+        minus[x].onclick = function () {
+            if (counter[i][x] > 0) {
+                counter[i][x]--;
+                number[i].getElementsByClassName("dropdown__visitors")[x].value = counter[i][x];
+                allGuests[i] = allGuests[i]-1;
+            }
+
+            setTitle();
         }
     }
-    let number = document.getElementsByClassName('dropdown__list')
-    let counter= [];
+}
 
-    for (let i = 0; i < acc.length; i++) {
-        quantity = number[i].getElementsByClassName("dropdown__visitors");
-        minus = number[i].getElementsByClassName("dropdown__minus");
-        plus = number[i].getElementsByClassName("dropdown__plus");
-        for (let x = 0; x < 3; x++) {
-            counter[x]= quantity[x].value;
-            plus[x].onclick = function () {
-                if (counter[x] < 100) {
-                    counter[x]++;
-                    number[i].getElementsByClassName("dropdown__visitors")[x].value = counter[x];
-                    allGuests[i] = allGuests[i]+1;}
-                setTitle();
+let title = document.getElementsByClassName("dropdown__name");
+let btnClear = document.getElementsByClassName("dropdown__clear");
 
-            };
-            minus[x].onclick = function () {
-                if (counter[x] > 0) {
-                    counter[x]--;
-                    number[i].getElementsByClassName("dropdown__visitors")[x].value = counter[x];
-                    allGuests[i] = allGuests[i]-1;
-                }
-
-                setTitle();
-            }
-        }
-    }
-
-    let title = document.getElementsByClassName("dropdown__name");
-    let btnClear = document.getElementsByClassName("dropdown__clear");
-
-    function setTitle() {
-        for(let i=0; i<acc.length; i++){
+function setTitle() {
+    for(let i=0; i<acc.length; i++){
 
         if (allGuests[i] > 0) {
             if (allGuests[i] === 1) {
@@ -82,19 +83,19 @@
         }
     }}
 
-    for(let i=0; i<acc.length; i++) {
-        let btnSubmit = document.getElementsByClassName('dropdown__submit')
-        btnClear[i].onclick = function () {
-            let a = 0;
-            while (a < 3){
+for(let i=0; i<acc.length; i++) {
+    let btnSubmit = document.getElementsByClassName('dropdown__submit')
+    btnClear[i].onclick = function () {
+        let a = 0;
+        while (a < 3){
             number[i].getElementsByClassName("dropdown__visitors")[a].value = 0;
             allGuests[a]=0;
-            counter[a]=0;
+            counter[i][a]=0;
             a++;}
-            title[i].innerHTML = 'Сколько гостей';
-            btnClear[i].style.visibility = "hidden";
-        }
-        btnSubmit[i].onclick = function () {
-            number[i].style.display = 'none';
-        }
+        title[i].innerHTML = 'Сколько гостей';
+        btnClear[i].style.visibility = "hidden";
     }
+    btnSubmit[i].onclick = function () {
+        number[i].style.display = 'none';
+    }
+}
