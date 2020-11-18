6faa16c6666749b4bf5a61e13313abebf9538c50
Yurchak Veronika <yurchakveronika1997@gmail.com>
2020-11-18 12:28:34 +0300

    Made headers&footers UI-kit

--- src/Blocks/DateDropdown/rangeCalendar.js
+++ src/Blocks/DateDropdown/rangeCalendar.js
@@ -1,20 +1,20 @@
-let inputDate = document.getElementsByClassName("datepicker-here")
+let inputDate = document.getElementsByClassName("date")
 let calendar = document.getElementsByClassName("datepicker")
 for(let i=0; i<inputDate.length; i++){
-    let inputFormDate = document.getElementsByClassName("datepicker__dates")
+    let inputFormDate = inputDate[i].getElementsByClassName("datepicker__dates")[0]
     calendar[i].addEventListener("click", function () {
         let numberFrom =calendar[i].getElementsByClassName("-range-from-")[0].dataset.date
         let numberTo =calendar[i].getElementsByClassName("-range-to-")[0].dataset.date
-        let monthFrom =calendar[i].getElementsByClassName("-range-from-")[0].dataset.month
-        let monthTo =calendar[i].getElementsByClassName("-range-to-")[0].dataset.month
+        let monthFrom =Number(calendar[i].getElementsByClassName("-range-from-")[0].dataset.month)+1
+        let monthTo =Number(calendar[i].getElementsByClassName("-range-to-")[0].dataset.month)+1
         let yearFrom =calendar[i].getElementsByClassName("-range-from-")[0].dataset.year
         let yearTo =calendar[i].getElementsByClassName("-range-to-")[0].dataset.year
-        inputFormDate[i].getElementsByClassName("datepicker__datefrom")[0].innerHTML= numberFrom+"."+monthFrom+"."+yearFrom;
-        inputFormDate[i].getElementsByClassName("datepicker__dateto")[0].innerHTML= numberTo+"."+monthTo+"."+yearTo;
+        inputFormDate.getElementsByClassName("datepicker__datefrom")[0].innerHTML= numberFrom+"."+monthFrom+"."+yearFrom;
+        inputFormDate.getElementsByClassName("datepicker__dateto")[0].innerHTML= numberTo+"."+monthTo+"."+yearTo;
     })
     let btnClear=calendar[i].getElementsByClassName("datepicker--buttons")
     btnClear[i].addEventListener("click", function () {
-        inputFormDate[i].getElementsByClassName("datepicker__datefrom")[0].innerHTML= "ДД.ММ.ГГГГ"
-        inputFormDate[i].getElementsByClassName("datepicker__dateto")[0].innerHTML= "ДД.ММ.ГГГГ"
+        inputFormDate.getElementsByClassName("datepicker__datefrom")[0].innerHTML= "ДД.ММ.ГГГГ"
+        inputFormDate.getElementsByClassName("datepicker__dateto")[0].innerHTML= "ДД.ММ.ГГГГ"
     })
 }
