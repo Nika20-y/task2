96aacbe8308178ef5bf31f9eec3628705bbcf2d4
Yurchak Veronika <yurchakveronika1997@gmail.com>
2021-02-02 13:05:22 +0300

    Corrected mistake on the page

--- src/Blocks/DateDropdown/rangeCalendar.js
+++ src/Blocks/DateDropdown/rangeCalendar.js
@@ -1,13 +1,18 @@
+
 let calendar = document.getElementsByClassName("datepicker")
 let inputFormDate = document.getElementsByClassName("datepicker__dates")
 for(let i=0; i<inputFormDate.length; i++){
-    calendar[i].addEventListener("click", function () {
-        let numberFrom =calendar[i].getElementsByClassName("-range-from-")[0].dataset.date;
-        let numberTo =calendar[i].getElementsByClassName("-range-to-")[0].dataset.date;
-        let monthFrom =Number(calendar[i].getElementsByClassName("-range-from-")[0].dataset.month)+1;
-        let monthTo =Number(calendar[i].getElementsByClassName("-range-to-")[0].dataset.month)+1;
-        let yearFrom =calendar[i].getElementsByClassName("-range-from-")[0].dataset.year;
-        let yearTo =calendar[i].getElementsByClassName("-range-to-")[0].dataset.year;
+    let a = i;
+    if(document.querySelector('.datepicker-here.datepicker-inactive') !== null){
+        a++;
+    }
+    calendar[a].addEventListener("click", function () {
+        let numberFrom =calendar[a].getElementsByClassName("-range-from-")[0].dataset.date;
+        let numberTo =calendar[a].getElementsByClassName("-range-to-")[0].dataset.date;
+        let monthFrom =Number(calendar[a].getElementsByClassName("-range-from-")[0].dataset.month)+1;
+        let monthTo =Number(calendar[a].getElementsByClassName("-range-to-")[0].dataset.month)+1;
+        let yearFrom =calendar[a].getElementsByClassName("-range-from-")[0].dataset.year;
+        let yearTo =calendar[a].getElementsByClassName("-range-to-")[0].dataset.year;
         monthFrom = monthFrom < 10 ? '0' + monthFrom : monthFrom;
         numberFrom = numberFrom < 10 ? '0' + numberFrom : numberFrom;
         monthTo = monthTo < 10 ? '0' + monthTo : monthTo;
